d488658ccfe046c04e3dff339a9dcb3e745d428e
DBook111 <zzlbook@163.com>
2025-01-16 23:08:04 +0800

    1.16

---
+++ BinaryTree.py
@@ -0,0 +1,8 @@
+class TreeNode:
+    "二叉树类节点"
+    def __init__(self, val: int):
+        self.val = val
+        self.left: TreeNode | None = None
+        self.right: TreeNode | None = None
+    
+    
