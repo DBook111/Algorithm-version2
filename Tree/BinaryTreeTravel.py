3aa896fa3ec2f8afc0117f3adbd90d2db41d45e9
DBook111 <zzlbook@163.com>
2025-01-28 09:34:54 +0800

    1.28

--- Tree/BinaryTreeTravel.py
+++ Tree/BinaryTreeTravel.py
@@ -1,7 +1,21 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, val: int):
         self.val: int = val 
         self.left: TreeNode | None = None
         self.right: TreeNode | None = None
 
-def 
+def level_order(root: TreeNode | None) -> list[int]:
+    """层序遍历"""
+    queue: deque[TreeNode] = deque()
+    queue.append(root)
+    res = []
+    while queue:
+        node: TreeNode | None = queue.popleft()
+        res.append(node.val)
+        if node.left is not None:
+            queue.append(node.left)
+        if node.right is not None:
+            queue.append(node.right)
+    return res
