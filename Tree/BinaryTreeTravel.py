3263b62809cbc2dbab895af27411e2c4d1ce9b76
DBook111 <zzlbook@163.com>
2025-01-28 09:58:02 +0800

    1.28

--- Tree/BinaryTreeTravel.py
+++ Tree/BinaryTreeTravel.py
@@ -19,3 +19,30 @@ def level_order(root: TreeNode | None) -> list[int]:
         if node.right is not None:
             queue.append(node.right)
     return res
+
+res = []
+
+def pre_order(root: TreeNode | None):
+    if root is None:
+        return
+    res.append(root.val)
+    pre_order(root.left)
+    pre_order(root.right)
+    
+def in_order(root: TreeNode | None):
+    """中序遍历"""
+    if root is None:
+        return
+    # 访问优先级：左子树 -> 根节点 -> 右子树
+    in_order(root=root.left)
+    res.append(root.val)
+    in_order(root=root.right)
+
+def post_order(root: TreeNode | None):
+    """后序遍历"""
+    if root is None:
+        return
+    # 访问优先级：左子树 -> 右子树 -> 根节点
+    post_order(root=root.left)
+    post_order(root=root.right)
+    res.append(root.val)    
