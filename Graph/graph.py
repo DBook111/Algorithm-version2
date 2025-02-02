3651d812e3c5f0a0da551261a9d0c2d5e6981e5f
DBook111 <zzlbook@163.com>
2025-02-02 16:39:16 +0800

    2.2

--- Graph/graph.py
+++ Graph/graph.py
@@ -60,6 +60,56 @@ class GraphAdjMat:
         print("邻接矩阵 = ")
         print_matrix(self.adj_mat)  
 
+class Vertex:
+    """顶点类"""
+    def __init__(self, val: int):
+        self.val = val
+
+class GraphAdjList:
+    """基于邻接表实现的无向图类"""
+    def __init__(self, edges: list[list[Vertex]]):
+        # 邻接表，key: 顶点, value: 该顶点的所有邻接顶点
+        self.adj_list = dict[Vertex, list[Vertex]]() # 创建一个空字典
+        # 添加所有的顶点和边
+        for edge in edges:
+            self.add_vertex(edge[0])
+            self.add_vertex(edge[1])
+            self.add_edge(edge[0], edge[1])
+    
+    def size(self) -> int:
+        return len(self.adj_list)
+    
+    def add_edge(self, vet1: Vertex, vet2: Vertex):
+        """添加边"""
+        if vet1 not in self.adj_list or vet2 not in self.adj_list or vet1 == vet2:
+            raise ValueError()
+        self.adj_list[vet1].append(vet2)
+        self.adj_list[vet2].append(vet1)
+        
+    def remove_edge(self, vet1: Vertex, vet2: Vertex):
+        """删除边"""
+        if vet1 not in self.adj_list or vet2 not in self.adj_list or vet1 == vet2:
+            raise ValueError()
+        self.adj_list[vet1].remove(vet2)
+        self.adj_list[vet2].remove(vet1)
+    
+    def add_vertex(self, vet: Vertex):
+        """添加顶点"""
+        if vet in self.adj_list:
+            raise ValueError()
+        self.adj_list[vet] = []
+    
+    def remove_vertex(self, vet: Vertex):
+        """删除顶点"""
+        if vet not in self.adj_list:
+            raise ValueError()
+        self.adj_list.pop(vet)
+        for vertex in self.adj_list:
+            for vet in self.adj_list[vertex]:
+                self.adj_list[vertex].remove(vet)
+
+        
+
 """Driver Code"""
 if __name__ == "__main__":
     # 初始化无向图
