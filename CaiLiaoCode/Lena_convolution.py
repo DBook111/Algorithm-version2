0a18d3ed0a70a005782c8083d0efa3ecf35d3cd9
book <zzlbook@163.com>
2025-05-17 15:33:00 +0800

    5.17

--- CaiLiaoCode/Lena_convolution.py
+++ CaiLiaoCode/Lena_convolution.py
@@ -1,12 +1,15 @@
+import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.metrics import structural_similarity as ssim
 
-# img_path = "D:\SoftWare\BaiduNetdisk\BaiduNetdiskDownload\cifar_png\cifar\\test\\13_horse.png"
-# img_path = "D:\SoftWare\BaiduNetdisk\BaiduNetdiskDownload\cifar_png\cifar\\test\\10_airplane.png"
-img_path = "C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\13_horse.png"
-class_name = img_path.split('\\')[-1].split('_')[1].split('.')[0]
+Combination_name = "Combination_4"
+
+# img_path = r"C:\Yan3\Algorithm-version2\CaiLiaoCode\input\235_airplane.png"
+# img_path = r"C:\Yan3\Algorithm-version2\CaiLiaoCode\input\330_automobile.png"
+img_path = r"C:\Yan3\Algorithm-version2\CaiLiaoCode\input\000000005836.jpg"
+class_name = img_path.split('\\')[-1].split('.')[0]
 # 读取灰度图像
 img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 # cv2.imwrite(f"D:\\gray_{class_name}.png", img)
@@ -25,32 +28,110 @@ sobel_kernel_z =  np.array([[0, 0, 0],
                            [0, 1, 0],
                            [0, 0, -1]], dtype=np.float32)
 
-# 1 外延
-sobel_kernel_x_industry_1 = np.array([[1.108e-10, 2.3731e-11, -1.108e-10],
-                           [1.108e-10, 2.3731e-11, -1.108e-10],
-                           [1.108e-10, 2.3731e-11, -1.108e-10]], dtype=np.float32)
+if Combination_name == "Combination_1":
+    # 1 外延
+    sobel_kernel_x_industry_1 = np.array([[1.108e-10, 2.3731e-11, -1.108e-10],
+                            [1.108e-10, 2.3731e-11, -1.108e-10],
+                            [1.108e-10, 2.3731e-11, -1.108e-10]], dtype=np.float32)
 
-sobel_kernel_y_industry_1 = np.array([[1.108e-10, 1.108e-10, 1.108e-10],
-                           [2.3731e-11, 2.3731e-11, 2.3731e-11],
-                           [-1.108e-10, -1.108e-10, -1.108e-10]], dtype=np.float32)
+    sobel_kernel_y_industry_1 = np.array([[1.108e-10, 1.108e-10, 1.108e-10],
+                            [2.3731e-11, 2.3731e-11, 2.3731e-11],
+                            [-1.108e-10, -1.108e-10, -1.108e-10]], dtype=np.float32)
 
-sobel_kernel_z_industry_1 =  np.array([[2.3731e-11, 2.3731e-11, 2.3731e-11],
-                           [2.3731e-11, 1.108e-10, 2.3731e-11],
-                           [2.3731e-11, 2.3731e-11, -1.108e-10]], dtype=np.float32)
+    sobel_kernel_z_industry_1 =  np.array([[2.3731e-11, 2.3731e-11, 2.3731e-11],
+                            [2.3731e-11, 1.108e-10, 2.3731e-11],
+                            [2.3731e-11, 2.3731e-11, -1.108e-10]], dtype=np.float32)
+    # 2 非外延
+    sobel_kernel_x_industry_2 = np.array([[1.80e-10, 1.75e-10, -1.80e-10],
+                            [1.80e-10, 1.75e-10, -1.80e-10],
+                            [1.80e-10,1.75e-10, -1.80e-10]], dtype=np.float32)
 
+    sobel_kernel_y_industry_2 = np.array([[1.80e-10, 1.80e-10, 1.80e-10],
+                            [1.75e-11, 1.75e-11, 1.75e-11],
+                            [-1.80e-10, -1.80e-10, -1.80e-10]], dtype=np.float32)
 
-# 2 非外延
-sobel_kernel_x_industry_2 = np.array([[1.80e-10, 1.75e-10, -1.80e-10],
-                           [1.80e-10, 1.75e-10, -1.80e-10],
-                           [1.80e-10,1.75e-10, -1.80e-10]], dtype=np.float32)
+    sobel_kernel_z_industry_2 = np.array([[1.75e-10, 1.75e-10, 1.75e-10],
+                            [1.75e-10, 1.80e-10, 1.75e-10],
+                            [1.75e-10, 1.75e-10, -1.80e-10]], dtype=np.float32)
+elif Combination_name == "Combination_2":
+    # 1 外延
+    sobel_kernel_x_industry_1 = np.array([[2.738e-10, 2.3731e-11, -2.738e-10],
+                            [2.738e-10, 2.3731e-11, -2.738e-10],
+                            [2.738e-10, 2.3731e-11, -2.738e-10]], dtype=np.float32)
 
-sobel_kernel_y_industry_2 = np.array([[1.80e-10, 1.80e-10, 1.80e-10],
-                           [1.75e-11, 1.75e-11, 1.75e-11],
-                           [-1.80e-10, -1.80e-10, -1.80e-10]], dtype=np.float32)
+    sobel_kernel_y_industry_1 = np.array([[2.738e-10, 2.738e-10, 2.738e-10],
+                            [2.3731e-11, 2.3731e-11, 2.3731e-11],
+                            [-2.738e-10, -2.738e-10, -2.738e-10]], dtype=np.float32)
 
-sobel_kernel_z_industry_2 = np.array([[1.75e-10, 1.75e-10, 1.75e-10],
-                           [1.75e-10, 1.80e-10, 1.75e-10],
-                           [1.75e-10, 1.75e-10, -1.80e-10]], dtype=np.float32)
+    sobel_kernel_z_industry_1 =  np.array([[2.3731e-11, 2.3731e-11, 2.3731e-11],
+                            [2.3731e-11, 2.738e-10, 2.3731e-11],
+                            [2.3731e-11, 2.3731e-11, -2.738e-10]], dtype=np.float32)
+
+    # 2 非外延
+    sobel_kernel_x_industry_2 = np.array([[2.16e-10, 1.75e-10, -2.16e-10],
+                           [2.16e-10, 1.75e-10, -2.16e-10],
+                           [2.16e-10,1.75e-10, -2.16e-10]], dtype=np.float32)
+
+    sobel_kernel_y_industry_2 = np.array([[2.16e-10, 2.16e-10, 2.16e-10],
+                            [1.75e-11, 1.75e-11, 1.75e-11],
+                            [-2.16e-10, -2.16e-10, -2.16e-10]], dtype=np.float32)
+
+    sobel_kernel_z_industry_2 = np.array([[1.75e-10, 1.75e-10, 1.75e-10],
+                            [1.75e-10, 2.16e-10, 1.75e-10],
+                            [1.75e-10, 1.75e-10, -2.16e-10]], dtype=np.float32)
+elif Combination_name == "Combination_3":
+    # 1 外延
+    sobel_kernel_x_industry_1 = np.array([[1.125e-9, 2.3731e-11, -1.125e-9],
+                            [1.125e-9, 2.3731e-11, -1.125e-9],
+                            [1.125e-9, 2.3731e-11, -1.125e-9]], dtype=np.float32)
+
+    sobel_kernel_y_industry_1 = np.array([[1.125e-9, 1.125e-9, 1.125e-9],
+                            [2.3731e-11, 2.3731e-11, 2.3731e-11],
+                            [-1.125e-9, -1.125e-9, -1.125e-9]], dtype=np.float32)
+
+    sobel_kernel_z_industry_1 =  np.array([[2.3731e-11, 2.3731e-11, 2.3731e-11],
+                            [2.3731e-11, 1.125e-9, 2.3731e-11],
+                            [2.3731e-11, 2.3731e-11, -1.125e-9]], dtype=np.float32)
+    # 2 非外延
+    sobel_kernel_x_industry_2 = np.array([[6.04e-10, 1.75e-10, -6.04e-10],
+                           [6.04e-10, 1.75e-10, -6.04e-10],
+                           [6.04e-10,1.75e-10, -6.04e-10]], dtype=np.float32)
+
+    sobel_kernel_y_industry_2 = np.array([[6.04e-10, 6.04e-10, 6.04e-10],
+                            [1.75e-11, 1.75e-11, 1.75e-11],
+                            [-6.04e-10, -6.04e-10, -6.04e-10]], dtype=np.float32)
+
+    sobel_kernel_z_industry_2 = np.array([[1.75e-10, 1.75e-10, 1.75e-10],
+                            [1.75e-10, 6.04e-10, 1.75e-10],
+                            [1.75e-10, 1.75e-10, -6.04e-10]], dtype=np.float32)
+elif Combination_name == "Combination_4":
+    # 1 外延
+    sobel_kernel_x_industry_1 = np.array([[1.771e-9, 2.3731e-11, -1.771e-9],
+                           [1.771e-9, 2.3731e-11, -1.771e-9],
+                           [1.771e-9, 2.3731e-11, -1.771e-9]], dtype=np.float32)
+
+    sobel_kernel_y_industry_1 = np.array([[1.771e-9, 1.771e-9, 1.771e-9],
+                            [2.3731e-11, 2.3731e-11, 2.3731e-11],
+                            [-1.771e-9, -1.771e-9, -1.771e-9]], dtype=np.float32)
+
+    sobel_kernel_z_industry_1 =  np.array([[2.3731e-11, 2.3731e-11, 2.3731e-11],
+                            [2.3731e-11, 1.771e-9, 2.3731e-11],
+                            [2.3731e-11, 2.3731e-11, -1.771e-9]], dtype=np.float32)
+    # 2 非外延
+    sobel_kernel_x_industry_2 = np.array([[1.06e-9, 1.75e-10, -1.06e-9],
+                           [1.06e-9, 1.75e-10, -1.06e-9],
+                           [1.06e-9,1.75e-10, -1.06e-9]], dtype=np.float32)
+
+    sobel_kernel_y_industry_2 = np.array([[1.06e-9, 1.06e-9, 1.06e-9],
+                            [1.75e-11, 1.75e-11, 1.75e-11],
+                            [-1.06e-9, -1.06e-9, -1.06e-9]], dtype=np.float32)
+
+    sobel_kernel_z_industry_2 = np.array([[1.75e-10, 1.75e-10, 1.75e-10],
+                            [1.75e-10, 1.06e-9, 1.75e-10],
+                            [1.75e-10, 1.75e-10, -1.06e-9]], dtype=np.float32)
+else:
+    raise ValueError(f"Combination_name: {Combination_name} is not valid")
+
 
 
 # 使用自定义卷积核计算梯度
@@ -97,61 +178,97 @@ sobelz_industry_cropped_2 = sobelz_industry_2[crop_size:-crop_size, crop_size:-c
 # ssim_z_2 = ssim(sobelz_cropped, sobelz_industry_cropped_2, data_range=sobelz_cropped.max() - sobelz_industry_cropped_2.min())
 
 # 合并六个数组
-combined_sobel = np.stack((sobelx_industry_cropped_1, 
-                           sobely_industry_cropped_1,
-                           sobelz_industry_cropped_1,
-                           sobelx_industry_cropped_2,
-                           sobely_industry_cropped_2,
-                           sobelz_industry_cropped_2), axis=-1)
+# 由于已经单独归一化每个数组，不再需要这一步
+# combined_sobel = np.stack((sobelx_industry_cropped_1, 
+#                          sobely_industry_cropped_1,
+#                          sobelz_industry_cropped_1,
+#                          sobelx_industry_cropped_2,
+#                          sobely_industry_cropped_2,
+#                          sobelz_industry_cropped_2), axis=-1)
 
 # 将结果归一化到 0-255 范围
 normalized_sobelx = cv2.normalize(sobelx_cropped, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 normalized_sobely = cv2.normalize(sobely_cropped, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 normalized_sobelz = cv2.normalize(sobelz_cropped, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 
-# normalized_sobelx_industry = cv2.normalize(sobelx_industry_cropped, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-# normalized_sobely_industry = cv2.normalize(sobely_industry_cropped, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-# normalized_sobelz_industry = cv2.normalize(sobelz_industry_cropped, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-normalized_combined_sobel = cv2.normalize(combined_sobel, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+# 分别归一化每个工业版Sobel结果
+normalized_sobelx_industry_1 = cv2.normalize(sobelx_industry_cropped_1, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+normalized_sobely_industry_1 = cv2.normalize(sobely_industry_cropped_1, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+normalized_sobelz_industry_1 = cv2.normalize(sobelz_industry_cropped_1, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+normalized_sobelx_industry_2 = cv2.normalize(sobelx_industry_cropped_2, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+normalized_sobely_industry_2 = cv2.normalize(sobely_industry_cropped_2, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+normalized_sobelz_industry_2 = cv2.normalize(sobelz_industry_cropped_2, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
 
-normalized_sobelx_industry_1 = normalized_combined_sobel[..., 0]
-normalized_sobely_industry_1 = normalized_combined_sobel[..., 1]
-normalized_sobelz_industry_1 = normalized_combined_sobel[..., 2]
-normalized_sobelx_industry_2 = normalized_combined_sobel[..., 3]
-normalized_sobely_industry_2 = normalized_combined_sobel[..., 4]
-normalized_sobelz_industry_2 = normalized_combined_sobel[..., 5]
-
-# # 计算 结构相似性指数ssim
-# ssim_x_1 = ssim(normalized_sobelx, normalized_sobelx_industry_1, data_range=normalized_sobelx.max() - normalized_sobelx.min())
-# ssim_y_1 = ssim(normalized_sobely, normalized_sobely_industry_1, data_range=normalized_sobely.max() - normalized_sobely.min())
-# ssim_z_1 = ssim(normalized_sobelz, normalized_sobelz_industry_1, data_range=normalized_sobelz.max() - normalized_sobelz.min())
-# ssim_x_2 = ssim(normalized_sobelx, normalized_sobelx_industry_2, data_range=normalized_sobelx.max() - normalized_sobelx.min() )
-# ssim_y_2 = ssim(normalized_sobely, normalized_sobely_industry_2, data_range=normalized_sobely.max() - normalized_sobely.min())
-# ssim_z_2 = ssim(normalized_sobelz, normalized_sobelz_industry_2, data_range=normalized_sobelz.max() - normalized_sobelz.min())
-
-ssim_x_1 = ssim(normalized_sobelx, normalized_sobelx_industry_1, data_range=normalized_combined_sobel.max() - normalized_combined_sobel.min())
-ssim_y_1 = ssim(normalized_sobely, normalized_sobely_industry_1, data_range=normalized_combined_sobel.max() - normalized_combined_sobel.min())
-ssim_z_1 = ssim(normalized_sobelz, normalized_sobelz_industry_1, data_range=normalized_combined_sobel.max() - normalized_combined_sobel.min())
-ssim_x_2 = ssim(normalized_sobelx, normalized_sobelx_industry_2, data_range=normalized_combined_sobel.max() - normalized_combined_sobel.min())
-ssim_y_2 = ssim(normalized_sobely, normalized_sobely_industry_2, data_range=normalized_combined_sobel.max() - normalized_combined_sobel.min())
-ssim_z_2 = ssim(normalized_sobelz, normalized_sobelz_industry_2, data_range=normalized_combined_sobel.max() - normalized_combined_sobel.min())
-
-
-# # 将结果归一化到 0-255 范围
-# normalized_sobelx = cv2.normalize(sobelx, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-# normalized_sobely = cv2.normalize(sobely, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-# normalized_slbelz = cv2.normalize(sobelz, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+# 计算结构相似性指数SSIM，对每对图像使用其共同的数据范围
+ssim_x_1 = ssim(normalized_sobelx, normalized_sobelx_industry_1, data_range=255)
+ssim_y_1 = ssim(normalized_sobely, normalized_sobely_industry_1, data_range=255)
+ssim_z_1 = ssim(normalized_sobelz, normalized_sobelz_industry_1, data_range=255)
+ssim_x_2 = ssim(normalized_sobelx, normalized_sobelx_industry_2, data_range=255)
+ssim_y_2 = ssim(normalized_sobely, normalized_sobely_industry_2, data_range=255)
+ssim_z_2 = ssim(normalized_sobelz, normalized_sobelz_industry_2, data_range=255)
 
 # 保存到指定文件夹
-cv2.imwrite(f"C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\sobelx_{class_name}.png", normalized_sobelx)
-cv2.imwrite(f"C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\sobelx_industry_{class_name}_1.png", normalized_sobelx_industry_1)
-cv2.imwrite(f"C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\sobelx_industry_{class_name}_2.png", normalized_sobelx_industry_2)
-cv2.imwrite(f"C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\sobely_{class_name}.png", normalized_sobely)
-cv2.imwrite(f"C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\sobely_industry_{class_name}_1.png", normalized_sobely_industry_1)
-cv2.imwrite(f"C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\sobely_industry_{class_name}_2.png", normalized_sobely_industry_2)
-cv2.imwrite(f"C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\slbelz_{class_name}.png", normalized_sobelz)
-cv2.imwrite(f"C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\slbelz_industry_{class_name}_1.png", normalized_sobelz_industry_1)
-cv2.imwrite(f"C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\slbelz_industry_{class_name}_2.png", normalized_sobelz_industry_2)
+save_name = os.path.split(img_path)[-1].split('.')[0]
+current_dir = os.path.dirname(os.path.abspath(__file__))
+result_dir = os.path.join(current_dir, 'save_result', Combination_name, save_name)
+show_kernels = os.path.join(current_dir, 'save_result', Combination_name)
+os.makedirs(result_dir, exist_ok=True)
+
+# 将卷积核可视化为矩阵图像并保存
+def create_kernel_comparison(kernels, titles, filename):
+    rows = 3  # X, Y, Z方向
+    cols = 3  # 标准, 外延, 非外延
+    
+    plt.figure(figsize=(15, 12))
+    
+    for i in range(len(kernels)):
+        plt.subplot(rows, cols, i+1)
+        ax = plt.gca()
+        im = ax.imshow(kernels[i], cmap='viridis')
+        
+        # 添加数值标签到每个单元格
+        for r in range(kernels[i].shape[0]):
+            for c in range(kernels[i].shape[1]):
+                value = kernels[i][r, c]
+                if abs(value) < 0.001:
+                    # 科学计数法显示小数，保留四位有效数字
+                    text = f'{value:.4g}'
+                else:
+                    # 保留四位有效数字
+                    text = f'{value:.4g}'
+                ax.text(c, r, text, ha='center', va='center', 
+                       color='white' if abs(value) > 0.0001 else 'black',
+                       fontsize=8)
+        
+        plt.title(titles[i])
+        plt.colorbar(im)
+    
+    plt.tight_layout()
+    plt.savefig(filename, dpi=600)
+    plt.close()
+
+# 创建所有卷积核的比较图
+all_kernels = [
+    sobel_kernel_x, sobel_kernel_x_industry_1, sobel_kernel_x_industry_2,
+    sobel_kernel_y, sobel_kernel_y_industry_1, sobel_kernel_y_industry_2,
+    sobel_kernel_z, sobel_kernel_z_industry_1, sobel_kernel_z_industry_2
+]
+all_titles = [
+    'Standard Sobel X', 'WaiYan Sobel X', 'FeiWaiYan Sobel X',
+    'Standard Sobel Y', 'WaiYan Sobel Y', 'FeiWaiYan Sobel Y',
+    'Standard Sobel Z', 'WaiYan Sobel Z', 'FeiWaiYan Sobel Z'
+]
+create_kernel_comparison(all_kernels, all_titles, os.path.join(show_kernels, 'all_kernels.png'))
+
+cv2.imwrite(os.path.join(result_dir, f"sobelx_{class_name}.png"), normalized_sobelx)
+cv2.imwrite(os.path.join(result_dir, f"sobelx_industry_{class_name}_1.png"), normalized_sobelx_industry_1)
+cv2.imwrite(os.path.join(result_dir, f"sobelx_industry_{class_name}_2.png"), normalized_sobelx_industry_2)
+cv2.imwrite(os.path.join(result_dir, f"sobely_{class_name}.png"), normalized_sobely)
+cv2.imwrite(os.path.join(result_dir, f"sobely_industry_{class_name}_1.png"), normalized_sobely_industry_1)
+cv2.imwrite(os.path.join(result_dir, f"sobely_industry_{class_name}_2.png"), normalized_sobely_industry_2)
+cv2.imwrite(os.path.join(result_dir, f"sobelz_{class_name}.png"), normalized_sobelz)
+cv2.imwrite(os.path.join(result_dir, f"sobelz_industry_{class_name}_1.png"), normalized_sobelz_industry_1)
+cv2.imwrite(os.path.join(result_dir, f"sobelz_industry_{class_name}_2.png"), normalized_sobelz_industry_2)
 
 
 # 显示结果
@@ -168,6 +285,13 @@ text_contents = [
     f'SSIM Y Industry 2: {ssim_y_2:.4f}',
     f'SSIM Z Industry 1: {ssim_z_1:.4f}',
     f'SSIM Z Industry 2: {ssim_z_2:.4f}'
+]
+
+# 将文本内容重新组合为3个双行文本
+combined_text = [
+    f'SSIM X WaiYan: {ssim_x_1:.4f}\nSSIM X FeiWaiYan: {ssim_x_2:.4f}',
+    f'SSIM Y WaiYan: {ssim_y_1:.4f}\nSSIM Y FeiWaiYan: {ssim_y_2:.4f}',
+    f'SSIM Z WaiYan: {ssim_z_1:.4f}\nSSIM Z FeiWaiYan: {ssim_z_2:.4f}'
 ]
 
 print(text_contents)
@@ -200,7 +324,7 @@ for i in range(0, len(result) - 1, 3):  # 每次处理三张图
     # 插入文字
     plt.subplot((len(result) + 2) // 3, 4, row * 4 + 4)  # 最右列放文字
     plt.axis('off')  # 不显示坐标轴
-    plt.text(0.5, 0.5, text_contents[row], ha='center', va='center', fontsize=12)
+    plt.text(0.5, 0.5, combined_text[row], ha='center', va='center', fontsize=12)
 
 # # 显示最后一张图
 # plt.subplot((len(result) + 2) // 3, 4, len(result) + 1)  # 放在最后一个可用的格子
@@ -210,5 +334,6 @@ for i in range(0, len(result) - 1, 3):  # 每次处理三张图
 
 # 调整子图间距
 plt.tight_layout()
-plt.savefig(f"C:\\Yan3\\Algorithm-version2\\CaiLiaoCode\\mix_{class_name}.png", dpi=600)
-plt.show()
+plt.savefig(os.path.join(result_dir, f"mix_{class_name}.png"), dpi=600)
+# plt.show()
+print('finish!')
